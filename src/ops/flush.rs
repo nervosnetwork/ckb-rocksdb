d0ddc3858b7f07301deefc552301693ed196891d
zhangsoledad <787953403@qq.com>
2026-09-23 14:06:10 +0800

    Expose per-column-family flush for application retirement

--- src/ops/flush.rs
+++ src/ops/flush.rs
@@ -30,7 +30,6 @@ where
     }
 }
 
-#[allow(dead_code)]
 pub trait FlushCF {
     /// Flushes database memtables to SST files on the disk for a given column family.
     fn flush_cf_opt(&self, cf: &ColumnFamily, flushopts: &FlushOptions) -> Result<(), Error>;
